d6be5ac124f978f6039d2770764cb2bf954b8f00
rtharston <rtharston@gmail.com>
2016-06-08 02:48:16 -0700

    Added basic version of home screen, working quotes display with edit button and new quote button with prompts to save or cancel.

--- app/src/main/java/edu/groupawesome/quotetracker/MainActivity.java
+++ app/src/main/java/edu/groupawesome/quotetracker/MainActivity.java
@@ -6,22 +6,33 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.Toast;
+import android.widget.*;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+
+    // The view we will be working with in this activity
+    private ListView mQuotesListView;
+    private SearchView mSearchView;
+    private ArrayAdapter<String> mAdapter;
+
+    private List<String> mQuotesTitlesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // set up the Toolbar so we can have buttons
         final Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
 
-        // make the action bar show or hide on click
+        // make the action bar show or hide on screen click
         LinearLayout mainLayout = (LinearLayout) findViewById(R.id.main_view);
         mainLayout.setOnClickListener(new View.OnClickListener() {
               @Override
@@ -36,8 +47,14 @@ public class MainActivity extends AppCompatActivity {
               }
           });
 
-    }
+        // set up our search and list
+        mQuotesListView = (ListView) findViewById(R.id.list_view_test);
 
+        setUpSearchView();
+        populateListView();
+        registerClickCallbackOnListItem();
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -59,21 +76,43 @@ public class MainActivity extends AppCompatActivity {
                 return true;
 
             case R.id.action_add:
-                showConfimDialog();
+                showNewQuoteDialog();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
+    /* Set up the Search View. Change settings here. **/
+    private void setUpSearchView() {
+        mSearchView = (SearchView) findViewById(R.id.search_view_test);
+        assert mSearchView != null;
+        mSearchView.setOnQueryTextListener(this);
+        mSearchView.setIconifiedByDefault(false);
+        mSearchView.setSubmitButtonEnabled(false);
+    }
+
+    /* Fill List View with list of titles from static QuotesList. **/
+    private void populateListView() {
+        // we may want a new array adapter that can take Quotes and display them in a special way
+        // display a list of the Titles of the Quotes
+        mQuotesTitlesList = Quote.getQuoteTitlesList();
+
+        mAdapter = new ArrayAdapter<String>(this, R.layout.list_view_text, mQuotesTitlesList);
+        mQuotesListView.setAdapter(mAdapter);
+
+        // enable filtering on the ListView so we can use the search bar
+        mQuotesListView.setTextFilterEnabled(true);
+    }
+
     /* **/
-    private void showConfimDialog() {
+    private void showNewQuoteDialog() {
         new AlertDialog.Builder(this)
                 .setTitle(R.string.confirm_add_title)
                 .setMessage(R.string.confirm_add_text)
                 .setPositiveButton(R.string.confirm_add, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        saveNewQuote();
+                        createNewQuote();
                     }
                 })
                 .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
@@ -85,34 +124,56 @@ public class MainActivity extends AppCompatActivity {
                 .show();
     }
 
-    private void saveNewQuote() {
-        Toast.makeText(MainActivity.this, "We'll add the quote feature soon, I promise!", Toast.LENGTH_SHORT).show();
-    }
+    private void createNewQuote() {
+        Intent newQuoteInent = new Intent(MainActivity.this, QuoteDisplayActivity.class);
 
-    /** Called when user clicks SMS Test button. */
-    public void smsTestClick(View view) {
-//        Intent intentSMSTestActivity = new Intent(this, SMSTestActivity.class);
-//
-//        // start our SMSTestActivity
-//        startActivity(intentSMSActivity);
+        newQuoteInent.putExtra(QuoteDisplayActivity.NEW_QUOTE, true);
+        startActivity(newQuoteInent);
     }
 
 
-    /** Called when user clicks ListView Test button. */
-    public void listViewTestClick(View view) {
-        Intent intentListViewTestActivity = new Intent(this, ListViewSearchTestActivity.class);
+    /** Register a click callback for items in list to call Quote Display. BROKEN */
+    private void registerClickCallbackOnListItem() {
 
-        // start our ListViewSearchTestActivity
-        startActivity(intentListViewTestActivity);
+        // create a listener to check for the item clicked and call the other activity
+        mQuotesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View viewClicked, int position, long id) {
+
+                Intent showQuoteIntent = new Intent(MainActivity.this, QuoteDisplayActivity.class);
+
+                // Here I am sending an int representing which item in the list was clicked.
+                // THIS IS FLAWED I KNOW - this assumes that the list the user clicked on is an exhaustive list of all the
+                // quotes, and thus the index of the list lines up perfectly with our QuotesList, but if it were a search
+                // then the list would only include some of the items and the index of the item on the ListView wouldn't
+                // match up with the actual index of the quote in the list - FOR THIS TEST IT WORKS, BUT NOT FOR THE REAL THING
+                // We'll have to find a new way to tell which quote the user clicked on.
+                showQuoteIntent.putExtra(Quote.QUOTE_LIST_INDEX, position);
+
+                // broadcast our showQuoteIntent
+                startActivity(showQuoteIntent);
+            }
+        });
+
     }
 
+    /* Call onQueryTextChange and just do the same thing. **/
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return onQueryTextChange(query);
+    }
 
-    /** Called when user clicks Another Test button. */
-    public void anotherTestClick(View view) {
-//        Intent intentSomeTestActivity = new Intent(this, SomeTestActivity.class);
+    /* Filter the listView with the text in search bar. **/
+    @Override
+    public boolean onQueryTextChange(String newText) {
 
-        // start our SomeTestActivity
-//        startActivity(intentSomeTestActivity);
+        if (TextUtils.isEmpty(newText)) {
+            mQuotesListView.clearTextFilter();
+        } else {
+            mQuotesListView.setFilterText(newText);
+        }
+        return true;
+
     }
 
 
--- app/src/main/java/edu/groupawesome/quotetracker/Quote.java
+++ app/src/main/java/edu/groupawesome/quotetracker/Quote.java
@@ -10,15 +10,16 @@ import java.util.List;
  */
 class Quote {
 
+    // A big hunk of test used for testing
     private static final String longText =
             "Material is the metaphor.\n\n A material metaphor is the unifying theory of a rationalized space and a system of motion. The material is grounded in tactile reality, inspired by the study of paper and ink, yet technologically advanced and open to imagination and magic.\n Surfaces and edges of the material provide visual cues that are grounded in reality. The use of familiar tactile attributes helps users quickly understand affordances. Yet the flexibility of the material creates new affordances that supercede those in the physical world, without breaking the rules of physics.\n The fundamentals of light, surface, and movement are key to conveying how objects move, interact, and exist in space and in relation to each other. Realistic lighting shows seams, divides space, and indicates moving parts.\n";
 
 
-    // public variables
+    // public variables for intents, used for testing
     final static String QUOTE_LIST_INDEX = "edu.teamawesome.cs246.quotes.QUOTE_LIST_INDEX";
     final static int TITLE_LENGTH = 10;  // maybe make this not final so the user can set the length in settings
 
-
+    // for creating a date and time stamp in generic title
     // set this up once so we don't keep making copies of it all the time **the S is for testing only
     final static private SimpleDateFormat sdf = new SimpleDateFormat("MMM d, y H:m:s:SSS a");
 
@@ -26,60 +27,54 @@ class Quote {
     // This list of quotes is stored where everyone can access it, and any new Quote gets added to it automatically
     // in the constructor. ** When we move to a DB then this will have to die, but we could still probably
     // add 'this' to the DB the same way 'this' is added to this List.
-    private static List<Quote> quotesList = new ArrayList<Quote>();;
+    private static List<Quote> quotesList = new ArrayList<Quote>();
     private static List<String> quoteTitlesList;  // maybe make a custom addQuote that automatically adds the title too... or something
 
     // FOR TESTING PURPOSES ONLY
-    static {
-        // create a list of 15 "Hello world!" strings for our ListView test
-        for (int i = 0; i < 15; i++) {
-            Quote newQuote = new Quote(longText); //, "Hello " + (i+1));
-
-            // sleep just a little bit so we can see the difference in time in the names
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+//    static {
+//        // create a list of 15 "Hello world!" strings for our ListView test
+//        for (int i = 0; i < 15; i++) {
+//            Quote newQuote = new Quote(longText, null, null, null); //, "Hello " + (i+1));
+//
+//            // sleep just a little bit so we can see the difference in time in the names
+//            try {
+//                Thread.sleep(10);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
 
     // constructors
     public Quote() {
-        // every new Quote we create will be added to the global list.
-        // ** This could be adapted to add to the DB I think **
-        quotesList.add(this);
+        this(null, null, null, null);
+    }
 
+    public Quote(String quote, String title, String author, String tag) {
+        // we have to have at least the quote text, the rest is optional
+//        assert(quote != null);
+        // LONGTEXT IS FOR TESTING ONLY
+        this.quote = (quote == null ? longText : quote);
+        this.title = (title == null ? generateTitle() : title);
+        this.tag = (tag == null ? "" : tag);
+        this.author = (author == null ? "" : author);
+
+        // add every new Quote to the list automatically for testing purposes
+//        quotesList.add(this);
+    }
+
+    private String generateTitle() {
         // get the current time and format it into a string and set it as the generic title
-        this.title = sdf.format(new Date().getTime());
-    }
-
-    public Quote(String quote) {
-        this();
-        this.quote = quote;
-    }
-
-    public Quote(String quote, String title) {
-        this();
-        this.quote = quote;
-        this.title = title;
-    }
-
-    public Quote(String quote, String title, String tag) {
-        this();
-        this.quote = quote;
-        this.title = title;
-        this.tag = tag;
+        return sdf.format(new Date().getTime());
     }
 
     // private variables
+    private String title;
     private String quote;
+    private String author;
 
     // fix this, I know it won't be a String
     private String tag;
-
-    private String title;
-
 
     // getters and setters
     public String getQuote() {
@@ -91,7 +86,7 @@ class Quote {
     }
 
     public String getTitle() {
-        // make this get the first word or two fo the quote, not the first so many letters
+        // make this get the first five words of the quote, not the first so many letters
         if (title == null) {
             title = quote.substring(0,TITLE_LENGTH);
         }
@@ -110,8 +105,25 @@ class Quote {
         this.tag = tag;
     }
 
-    public static List<Quote> getQuotesList() {
-        return quotesList;
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    // the idea was to retrieve from the quotesList without allowing it to be modified, but it didn't work
+//    public static List<Quote> getQuotesList() {
+//        return quotesList;
+//    }
+
+    public static Quote getQuoteAtIndex(int index) {
+        return quotesList.get(index);
+    }
+
+    public static boolean quotesListContains(Quote quote) {
+        return quotesList.contains(quote);
     }
 
     public static List<String> getQuoteTitlesList() {
@@ -124,5 +136,10 @@ class Quote {
         }
         return quoteTitlesList;
     }
+
+    public static void addToQuotesList(Quote newQuote) {
+        quotesList.add(newQuote);
+    }
+
 
 }
