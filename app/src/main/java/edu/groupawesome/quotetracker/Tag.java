eabff1422af1ca6789be7a4d56ca7bcf873bdea2
rtharston <rtharston@gmail.com>
2016-06-16 18:38:02 -0300

    All Tag class stubs complete. Testing still required.

--- app/src/main/java/edu/groupawesome/quotetracker/Tag.java
+++ app/src/main/java/edu/groupawesome/quotetracker/Tag.java
@@ -8,73 +8,112 @@ import java.util.*;
 public class Tag {
 
     private String mTagName;
-    private Set<String> mKeywords = new HashSet<String>();
-    private static List<Tag> sTagList = new ArrayList<Tag>();
+    private Set<String> mKeywordsSet = new HashSet<String>();
+    private static Set<Tag> sTagSet = new HashSet<Tag>();
 
     // we don't want nameless Tags
     private Tag() {}
 
-
-    public Tag(String name) {
-        assert (name != null);
-        mTagName = name;
+    /** Constructor: Name only, no keywords. */
+    public Tag(String newName) {
+        assert (newName != null);
+        mTagName = newName;
     }
 
-    public Tag(String name, List<String> keywords) {
-        this(name);
-        assert (keywords != null);
-        mKeywords.addAll(keywords);
+    /** Constructor: Name and keywords (Collection of Strings). */
+    public Tag(String newName, Collection<String> newKeywords) {
+        this(newName);
+        assert (newKeywords != null);
+        mKeywordsSet.addAll(newKeywords);
     }
-
 
     public String getTagName() {
+        assert (mTagName != null);
         return mTagName;
     }
 
-    public void changeTagName(String mTagName) {
-        assert (mTagName != null);
-        this.mTagName = mTagName;
+    public void changeTagName(String newTagName) {
+        assert (newTagName != null);
+        this.mTagName = newTagName;
     }
 
-//    public List<String> getKeywords() {
-//        return mKeywords;
-//    }
-//
-//    public void setmKeywords(List<String> mKeywords) {
-//        assert (mKeywords != null);
-//        this.mKeywords = mKeywords;
-//    }
-
-    public boolean addKeyword(String keyword) {
-        return mKeywords.add(keyword);
+    public boolean addKeyword(String newKeyword) {
+        assert (newKeyword != null);
+        assert (mKeywordsSet != null);
+        return mKeywordsSet.add(newKeyword);
     }
 
-    public boolean addAllKeywords(String[] keywords) {
-        return Collections.addAll(mKeywords, keywords);
+    public boolean addAllKeywords(String[] newKeywords) {
+        assert (newKeywords != null);
+        assert (mKeywordsSet != null);
+        return Collections.addAll(mKeywordsSet, newKeywords);
     }
 
-    public boolean addAllKeywords(Set<String> keywords) {
-        return mKeywords.addAll(keywords);
+    public boolean addAllKeywords(Collection<String> newKeywords) {
+        assert (newKeywords != null);
+        assert (mKeywordsSet != null);
+        return mKeywordsSet.addAll(newKeywords);
     }
 
     public boolean removeKeyword(String keyword) {
-        return mKeywords.remove(keyword);
+        assert (keyword != null);
+        assert (mKeywordsSet != null);
+        return mKeywordsSet.remove(keyword);
     }
 
     public boolean hasKeyword(String keyword) {
-        return mKeywords.contains(keyword);
+        assert (keyword != null);
+        assert (mKeywordsSet != null);
+        return mKeywordsSet.contains(keyword);
     }
 
-    public static List<Tag> getTagList() {
-        return sTagList;
+    // TODO: test this unmodifiable Set - I don't want someone changing mKeywordsSet externally
+    public Set<String> getKeywordsSet() {
+        assert (mKeywordsSet != null);
+        return Collections.unmodifiableSet(mKeywordsSet);
     }
 
-    public static void setTagList(List<Tag> sTagList) {
-        Tag.sTagList = sTagList;
+    /** Clears all the keywords and add the new Collection of Keywords*/
+    public void setKeywordsSet(Collection<String> keywords) {
+        // we don't want any null pointer exceptions here!
+        assert (keywords != null);
+        assert (mKeywordsSet != null);
+
+        // clear the current Set if it isn't empty already
+        if (!sTagSet.isEmpty()) { this.mKeywordsSet.clear(); }
+
+        // add all the keywords to the set
+        this.mKeywordsSet.addAll(mKeywordsSet);
     }
 
-    public static boolean addTagToTagsList(Tag newTag) {
-        return sTagList.add(newTag);
+    // TODO: test this unmodifiable Set - I don't want someone changing sTagsSet externally
+    public static Set<Tag> getTagSet() {
+        assert (sTagSet != null);
+        return Collections.unmodifiableSet(sTagSet);
+    }
+
+    /** Clears the Set of Tags and adds a whole new Collection. */
+    public static void setTagSet(Collection<Tag> tagSet) {
+        assert (tagSet != null);
+        assert (sTagSet != null);
+
+        // clear the current Set if it isn't empty already
+        if (!sTagSet.isEmpty()) { sTagSet.clear(); }
+
+        // add all the tags to the set
+        Tag.sTagSet.addAll(tagSet);
+    }
+
+    public static boolean addTagToTagsSet(Tag newTag) {
+        assert (newTag != null);
+        assert (sTagSet != null);
+        return sTagSet.add(newTag);
+    }
+
+    public static boolean removeTagFromTagsSet(Tag tag) {
+        assert (tag != null);
+        assert (sTagSet != null);
+        return sTagSet.remove(tag);
     }
 
 }
